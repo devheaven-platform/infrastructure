38d7b723a7a32e7797896dc25b26ceb10631c465
tomdewildt <t.dewildt@student.fontys.nl>
2019-03-26 12:35:09 +0100

    Add metallb via helm

---
+++ config/metallb/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This tool will setup metallb on a kubernetes cluster."
+echo "It expects that you have your cluster up and running and"
+echo "helm installed."
+echo
+
+echo "Installing metallb"
+helm install stable/metallb --name metallb --namespace metallb-system --values values.yaml
+echo
+
+echo "Done"
+echo
