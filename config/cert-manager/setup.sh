1a53f391a667fb7705f0b261cebd2223bf3aba53
tomdewildt <t.dewildt@student.fontys.nl>
2019-03-27 21:48:03 +0100

    Add cert-manager config

---
+++ config/cert-manager/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "This tool will setup cert-manager on a kubernetes cluster."
+echo "It expects that you have your cluster up and running and"
+echo "helm installed."
+echo
+
+echo "Installing CRD's"
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+echo
+
+echo "Installing cert-manager"
+helm install stable/cert-manager \
+    --name cert-manager \
+    --namespace cert-manager-system \
+    --values values.yaml
+echo
+
+echo "Done"
+echo
